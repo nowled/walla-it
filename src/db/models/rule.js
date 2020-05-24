4bcd2b3c0d741b6f901303bd78a226748ddf5162
nowled <nowled@excite.com>
2020-05-24 22:49:42 +0300

    Before Migrations Of Rules To Create The Table

--- src/db/models/rule.js
+++ src/db/models/rule.js
@@ -3,8 +3,12 @@ module.exports = (sequelize, DataTypes) => {
   const Rule = sequelize.define('Rule', {
     description: DataTypes.STRING
   }, {});
-  Rule.associate = function(models) {
+  Rule.associate = function (models) {
     // associations can be defined here
+    Rule.belongsTo(models.Topic, {
+      foreignKey: "topicId",
+      onDelete: "CASCADE",
+    });
   };
   return Rule;
 };
