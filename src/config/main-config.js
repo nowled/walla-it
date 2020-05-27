1cb39d1537561175f766fd9e6c04cff702762908
nowled <nowled@excite.com>
2020-05-27 20:53:04 +0300

    Finishing Up Topic 2

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -3,12 +3,18 @@ const express = require('express');
 const app = express();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParser = require("body-parser");
+
 
 
 module.exports = {
   init(app, express) {
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
+    app.use(bodyParser.urlencoded({
+      extended: true
+    }));
+
     app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 }
